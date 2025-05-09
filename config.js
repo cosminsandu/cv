4905c557d82751021f2062db89b57bf39fd43c25
Cosmin Sandu <cosmin@foodomarket.com>
2025-05-09 17:56:01 +0300

    Implement based on libertateinit template

---
+++ config.js
@@ -0,0 +1,357 @@
+// all the CV configurations are here
+var cvConfig = {
+    name: "Cosmin SANDU",
+    title: [
+        "Certified Symfony Developer",
+        "Senior PHP Developer",
+        "Freelancer"
+    ],
+    aboutMe: [
+        [
+            {type: "normal", content: "I am a Certified Symfony developer / Contractor / Freelancer with over "},
+            {type: "bold", content: "16+ years of experience"},
+            {type: "normal", content: ", only interested in #remote work."},
+        ],
+        [
+            {
+                type: "normal",
+                content: "I use my expertise, skills, and passion to design and implement top-of-the-line software solutions for companies worldwide."
+            },
+        ],
+        [
+            {type: "bold", content: "Drop me a message"},
+            {type: "normal", content: " if you think my expertise could help your organization!"},
+        ]
+    ],
+    contact: {
+        phone: "+40 724 822 801",
+        email: "mail@cosminsandu.ro",
+        location: "Craiova, România",
+        linkedin: "in/sanducosminmihai/",
+        skype: "Cosmin SANDU",
+        skypeLink: "https://join.skype.com/invite/EXexTXCn6W6X"
+    },
+    skills: [
+        {type: "normal", name: "PHP"},
+        {type: "normal", name: "Symfony"},
+        {type: "normal", name: "Git"},
+        {type: "normal", name: "Docker"},
+        {type: "normal", name: "MySQL"},
+        {type: "normal", name: "Jira"},
+        {type: "normal", name: "RabbitMQ"},
+        {type: "normal", name: "Redis"},
+        // {type: "normal", name: "Classic AUTOSAR"},
+        // {type: "normal", name: "Docker"},
+        // {type: "normal", name: "Jenkins"},
+        // {type: "normal", name: "Git"},
+        // {type: "normal", name: "Google Test"},
+        // {type: "normal", name: "Agile"},
+        // {type: "normal", name: "Surround SCM"},
+        // {type: "normal", name: "Tessy"},
+        // {type: "normal", name: "Polyspace"}
+        //{type: "custom", name: "example", years: 2},
+    ],
+    skillsYearsRoundAtMonths: 8,
+    education: [
+        {
+            university: "The University of Craiova",
+            degree: "Engineering degree, Computer Science",
+            locAndYears: "Craiova, 2003 - 2008"
+        },
+        {
+            university: "The University of Craiova",
+            degree: "Master's degree, Business Management",
+            locAndYears: "Craiova, 2009 - 2011"
+        },
+    ],
+    experience: [
+        {
+            pageBreak: false,
+            position: "Senior Backend Software Developer",
+            period: [{month: 7, year: 2024}, {month: 5, year: 2025}],
+            project: "Nexus Project",
+            company: "AVAG Holding SE",
+            description: "The client, a key player in the automotive retail market, was creating a new IT platform for AVAG’s future ",
+            impact: [
+                [
+                    {
+                        type: "normal",
+                        text: "Implement Symfony Skeleton (a Symfony template for all the microservices)"
+                    }
+                ],
+                [
+                    {
+                        type: "normal",
+                        text: "Implement several microservice: Foundation, Workshop Scheduler, Availability, Employee, Vehicle, Customer, DMS Connect, Notification, etc."
+                    }
+                ]
+            ],
+            skills: ["PHP", "Symfony", "DDD", "Swagger", "RabbitMQ", "Git", "Docker", "PostgreSQL", "Jira", "PHPUnit", "Codeception"],
+            skillsHidden: []
+        },
+        {
+            pageBreak: false,
+            position: "Senior Backend Software Developer",
+            period: [{month: 3, year: 2024}, {month: 6, year: 2024}],
+            project: "HeyBlu",
+            company: "Zitec",
+            description: "",
+            impact: [
+                [
+                    {
+                        type: "normal",
+                        text: "Implement Notification Service that sends emails, SMS and webhook notifications"
+                    }
+                ],
+                [
+                    {
+                        type: "normal",
+                        text: "Implement Loan Management Service that manage loans in a Core Bankins System"
+                    }
+                ]
+            ],
+            skills: ["PHP", "Symfony", "DDD", "AWS", "AWS", "PinPoint", "Docker", "MySQL", "Jira", "PHPUnit", "RabbitMQ"],
+            skillsHidden: []
+        },
+        {
+            pageBreak: false,
+            position: "Senior Backend Software Developer",
+            period: [{month: 1, year: 2024}, {month: 2, year: 2024}],
+            project: "X",
+            company: "Digt AG",
+            description: "",
+            impact: [
+                [
+                    {
+                        type: "normal",
+                        text: "Implement PIM (Product Information Management) application"
+                    }
+                ],
+            ],
+            skills: ["PHP", "Symfony", "DDD", "CQRS", "Git", "Docker", "MySQL", "Jira", "PHPUnit", "RabbitMQ"],
+            skillsHidden: []
+        },
+        {
+            pageBreak: true,
+            position: "Backend Software Developer",
+            period: [{month: 5, year: 2022}, {month: 12, year: 2023}],
+            project: "B2B Food Marketplace",
+            company: "Foodomarket",
+            description: "",
+            impact: [
+                [
+                    {
+                        type: "normal",
+                        text: "Led the backend team and served as Scrum Master"
+                    }
+                ],
+                [
+                    {
+                        type: "normal",
+                        text: "Implemented Geo zone microservice"
+                    }
+                ],
+                [
+                    {
+                        type: "normal",
+                        text: "Developed Notification service"
+                    }
+                ],
+                [
+                    {
+                        type: "normal",
+                        text: "Set up a logging system with live notifications for critical events"
+                    }
+                ],
+                [
+                    {
+                        type: "normal",
+                        text: "Conducted security checks and fixed vulnerabilities"
+                    }
+                ],
+                [
+                    {
+                        type: "normal",
+                        text: "Migrated main stack to PHP 8"
+                    }
+                ],
+                [
+                    {
+                        type: "normal",
+                        text: "Migrated main stack to PHP 8"
+                    }
+                ],
+                [
+                    {
+                        type: "normal",
+                        text: "Upgraded Symfony & Sylius versions"
+                    }
+                ],
+                [
+                    {
+                        type: "normal",
+                        text: "Contributed to creating a CI pipeline"
+                    }
+                ],
+            ],
+            skills: ["PHP", "Symfony", "Sylius", "Git", "Docker", "MySQL", "MariaDB", "MongoDB", "Jira", "RabbitMQ"],
+            skillsHidden: []
+        },
+        {
+            pageBreak: false,
+            position: "Technical Lead on Symfony",
+            period: [{month: 4, year: 2021}, {month: 4, year: 2022}],
+            project: "Newpharma",
+            company: "SmartValue",
+            description: "",
+            impact: [
+                [
+                    {
+                        type: "normal",
+                        text: "Led team to migrate to Symfony framework."
+                    }
+                ],
+                [
+                    {
+                        type: "normal",
+                        text: "Trained team in Symfony and Unit tests"
+                    }
+                ],
+                [
+                    {
+                        type: "normal",
+                        text: "Implemented a queueing system to handle large loads, ensuring system speed and scalability"
+                    }
+                ],
+                [
+                    {
+                        type: "normal",
+                        text: "Investigated production loads/performance/issues with the System Architect and DevOps"
+                    }
+                ],
+            ],
+            skills: ["PHP", "Symfony", "DDD", "MariaDB", "Git", "Docker", "MySQL", "Jira", "PHPUnit", "RabbitMQ", "Redis", "New Relic", "Blackfire"],
+            skillsHidden: []
+        },
+        {
+            pageBreak: false,
+            position: "Senior Software Developer",
+            period: [{month: 4, year: 2017}, {month: 3, year: 2021}],
+            project: "Marketplace",
+            company: "eMAG IT Research",
+            description: "",
+            impact: [
+                [
+                    {
+                        type: "normal",
+                        text: "Implemented eMAG Marketplace Account Health"
+                    }
+                ],
+                [
+                    {
+                        type: "normal",
+                        text: "Developed eMAG Marketplace Seller Invoice"
+                    }
+                ],
+                [
+                    {
+                        type: "normal",
+                        text: "Worked on eMAG Finance and Insurance Applications"
+                    }
+                ],
+                [
+                    {
+                        type: "normal",
+                        text: "Release Manager"
+                    }
+                ],
+            ],
+            skills: ["PHP", "Symfony", "SOLR", "ELK stack", "Git", "OpenStack", "MySQL", "Jira", "PHPUnit", "RabbitMQ", "Redis", "Memcache", "jQuery", "Jenkins"],
+            skillsHidden: []
+        },
+        {
+            pageBreak: true,
+            position: "Software Developer",
+            period: [{month: 5, year: 2012}, {month: 3, year: 2017}],
+            project: "Multiple projects",
+            company: "NetRom Software",
+            description: "",
+            impact: [
+                [
+                    {
+                        type: "normal",
+                        text: "Implemented telecom solution portals using Symfony and CodeIgniter"
+                    }
+                ],
+                [
+                    {
+                        type: "normal",
+                        text: "Developed SOAP & SOAP Proxy with management applications"
+                    }
+                ],
+                [
+                    {
+                        type: "normal",
+                        text: "Built PhoneManager backend (REST)"
+                    }
+                ],
+                [
+                    {
+                        type: "normal",
+                        text: "Developed fraud management solutions"
+                    }
+                ],
+                [
+                    {
+                        type: "normal",
+                        text: "Create healthcare solutions"
+                    }
+                ],
+                [
+                    {
+                        type: "normal",
+                        text: "Developed a job search solution"
+                    }
+                ],
+            ],
+            skills: ["PHP", "Symfony", "SOLR", "ELK stack", "Git", "OpenStack", "MySQL", "Jira", "PHPUnit", "RabbitMQ", "Redis", "Memcache", "jQuery", "Jenkins"],
+            skillsHidden: []
+        },
+        {
+            pageBreak: false,
+            position: "Software Quality Assurance Engineer",
+            period: [{month: 8, year: 2008}, {month: 2, year: 2012}],
+            project: "Multiple projects",
+            company: "NetRom Software",
+            description: "",
+            impact: [
+                [
+                    {
+                        type: "normal",
+                        text: "Conducted manual testing"
+                    }
+                ],
+                [
+                    {
+                        type: "normal",
+                        text: "Crawled for rental properties in the Netherlands"
+                    }
+                ],
+                [
+                    {
+                        type: "normal",
+                        text: "Performed Search Engine Optimization (SEO)"
+                    }
+                ],
+                [
+                    {
+                        type: "normal",
+                        text: "Conducted automated UI testing with TestComplete (JScript)"
+                    }
+                ],
+            ],
+            skills: [ "Jira", "TestComplete", "JScript", "SVN", "SEO", "Manual Testing", "Automatic Testing"],
+            skillsHidden: []
+        },
+    ]
+};
